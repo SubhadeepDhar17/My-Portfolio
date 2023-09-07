558c2fa3d873a7bfea8c6bd31133bc4bb89c0bb4
subhadeepdhar17 <oliverdepp17@gmail.com>
2023-09-07 21:35:26 +0530

    Extra Menu Added

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -1,12 +1,13 @@
 import {NavLink} from 'react-router-dom';
 import './menu.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Menu = () => {
     const [mobileMenu, setMobileMenu] = useState(false);
+    const [myMenuShow, setMyMenuShow] = useState(false);
 
     const Menu = [
         {
@@ -27,10 +28,33 @@ const Menu = () => {
         },
     ]
     const MenuMap = Menu.map((list) => <li><NavLink to={list.to}>{list.name}</NavLink></li>);
+
+    const MyMenu = [
+        {
+            icon: faLinkedin,
+            to: 'https://www.linkedin.com/in/subhadeep-dhar/',
+            target: '_blank',
+        },
+        {
+            icon: faGithub,
+            to: 'https://github.com/SubhadeepDhar17',
+            target: '_blank',
+        },
+        {
+            icon: faEnvelope,
+            to: '/',
+            target: '_blank',
+        }
+    ]
+    const MyMenuMap = MyMenu.map((myMenuList) =>
+    <li><NavLink to={myMenuList.to} target='_blank'>
+        <FontAwesomeIcon style={{borderRadius: '100px'}} icon={myMenuList.icon} />
+    </NavLink></li>)
+
     return (
         <>
             <div className='navBar'>
-                <div className='logo'>
+                <div className='logo' onClick={() => setMyMenuShow(!myMenuShow)}>
                     Subha
                 </div>
                 <div className='menuBar'>
@@ -44,9 +68,17 @@ const Menu = () => {
                     <li onClick={() => setMobileMenu(!mobileMenu)}
                         className={`hamburger ${mobileMenu ? 'vertical' : 'horizontal'}`} >
                         <FontAwesomeIcon icon={faBars} />
+                        {mobileMenu && myMenuShow ? setMobileMenu(!mobileMenu) : null}
                     </li>
-                    <li><h1><NavLink to='/'>Subha</NavLink></h1></li>
+                    <li>
+                        <div>
+                            <h1 onClick={() => setMyMenuShow(!myMenuShow)}>Subha</h1>
+                        </div>
+                    </li>
                 </ul>
+            </div>
+            <div className={`subhaMenu ${myMenuShow ? 'myMenuActive' : 'myMenuInactive'}`}>
+                {MyMenuMap}
             </div>
             <div className={`hamburgerMenu ${mobileMenu ? 'active' : 'inactive'}`}
                 onClick={() => setMobileMenu(!mobileMenu)}>
